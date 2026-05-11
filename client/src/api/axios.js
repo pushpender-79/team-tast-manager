87d491851cd038cd4dca452685bce9138436c423
pushpender-79 <pushpenderchauhan79@gmail.com>
2026-05-11 15:32:17 +0530

    fixed some url issues

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,36 +1,8 @@
-// import axios from 'axios'
-
-// const API = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// })
-
-// // Har request mein token automatically lagao
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem('token')
-//   if (token) config.headers.Authorization = `Bearer ${token}`
-//   return config
-// })
-
-// // 401 aane par logout karo
-// API.interceptors.response.use(
-//   (response) => response,
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       localStorage.removeItem('token')
-//       localStorage.removeItem('user')
-//       window.location.href = '/login'
-//     }
-//     return Promise.reject(error)
-//   }
-// )
-
-// export default API
-
 
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',  // ← sirf ye line change karo
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 // Har request mein token automatically lagao
